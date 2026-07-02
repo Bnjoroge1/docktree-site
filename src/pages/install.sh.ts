a231b0cc5c894fd27f848207ef57f7e57e995e8a
Bill Njoroge <williamsriunge@gmail.com>
2026-07-01 23:15:15 -0400

    Remove Node imports from installer endpoints

--- src/pages/install.sh.ts
+++ src/pages/install.sh.ts
@@ -1,8 +1,8 @@
 import type { APIRoute } from 'astro';
-import { readFileSync } from 'node:fs';
-import { join } from 'node:path';
 
-const installScript = readFileSync(join(process.cwd(), 'installer', 'install.sh'), 'utf8');
+import installScript from '../../installer/install.sh?raw';
+
+export const prerender = true;
 
 export const GET: APIRoute = () =>
   new Response(installScript, {
